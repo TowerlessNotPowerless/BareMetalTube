50c0bf273302a31a8cc1007b1e2ab16ba9400374
Sam <SamSkivington@users.noreply.github.com>
2026-08-23 13:01:32 +0100

    Added basic bits to makefile to prove makefile works

---
+++ src/common/assemblerDefs.asm
@@ -0,0 +1,19 @@
+.feature	labels_without_colons
+.feature	org_per_seg
+
+.case +
+
+.define	equ	=
+.define	db	.byte
+.define	dw	.word
+.define	dt	.faraddr
+.define	dd	.dword
+
+.zeropage
+.org 0
+
+.bss
+.org 0
+
+.code
+.org 0
---
+++ src/timestamp/main.asm
@@ -0,0 +1,4 @@
+.include "../common/coreDefs.asm"
+
+.code
+	dd	.time
